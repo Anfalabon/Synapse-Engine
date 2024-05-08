f9e31ae2a1d2599c69b088055dce41422d1f7c48
Anfal Abon <codinghub447@gmail.com>
2024-05-08 22:51:44 +0600

    Shifted main in dynamicArray.hpp to dynamicArray.cpp in tests directory

--- tests/dynamicArray.cpp
+++ tests/dynamicArray.cpp
@@ -1,2 +1,16 @@
 
 #include "dynamicArray.hpp"
+
+
+int main()
+{
+    Hilbert::DyArray<float> objects(3);
+
+    objects.add(1.0f);
+    objects.add(2.0f);
+    objects.add(3.0f);
+
+    objects.display();
+
+    return 0;
+}
--- tests/dynamicArray.hpp
+++ tests/dynamicArray.hpp
@@ -74,19 +74,6 @@ namespace Hilbert
 
 
 
-int main()
-{
-    Hilbert::DyArray<float> objects(3);
-
-    objects.add(1.0f);
-    objects.add(2.0f);
-    objects.add(3.0f);
-
-    objects.display();
-
-    return 0;
-}
-
 
 
 #endif //SIMULATION_DYNAMICARRAY_H
